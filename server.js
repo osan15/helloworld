afa93dbaf4264c1ce8464610e612c77426b096fe
osan15 <antoine.drian2@gmail.com>
2017-05-03 11:50:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ var server = http.createServer(function (request, response) {
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(port, '0.0.0.0');
+server.listen({
+  host: '0.0.0.0',
+  port: port
+});
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:"+port+"/");
