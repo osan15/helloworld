31c627ed776d9187a3fa5f46c14171f83cbd9e48
osan15 <antoine.drian2@gmail.com>
2017-05-03 11:47:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 // Load the http module to create an http server.
 var http = require('http');
+var port = 80;
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
@@ -9,7 +10,7 @@ var server = http.createServer(function (request, response) {
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8009);
+server.listen(port);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:80/");
+console.log("Server running at http://127.0.0.1:"+port+"/");
